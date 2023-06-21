facf101f89feab89c8df5c2e0be8c589c2feb703
Abhinav Kumar <kabhinav713@gmail.com>
2023-06-21 21:19:30 +0530

    [Abhinav]-adding part time

---
+++ PartTime.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage2
+{
+    public class PartTime
+    {
+        public void EmployeeWage()
+        {
+            int parttime = 1;
+            int fulltime = 2;
+            int wageperhr = 20;
+
+            int empwage = 0;
+            int emphrs = 0;
+
+            Random random = new Random();
+            int empcheck = random.Next(0, 3);
+            Console.WriteLine(empcheck);
+            if (empcheck == parttime)
+            {
+                emphrs = 8;
+                empwage = emphrs * wageperhr;
+                Console.WriteLine("Employee Part Time Wage--" + empwage);
+            }
+            else if (empcheck == fulltime)
+            {
+                emphrs = 16;
+                empwage = emphrs * wageperhr;
+                Console.WriteLine("Employee Full Time Wage--" + empwage);
+            }
+            else
+            {
+                emphrs = 0;
+                empwage = emphrs * wageperhr;
+                Console.WriteLine("Employee is Absent");
+            }
+
+
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,15 @@
 ﻿using EmployeeWage2;
 
-
-
-
-
 while (true)
 {
-    Console.WriteLine("Select the choice\n1)Employee Presence\n2)Employee Daily Wage");
+    Console.WriteLine("Select the choice\n1)Employee Presence\n2)Employee Daily Wage" +
+        "\n3)Part Time Wage");
     int choice = Convert.ToInt32(Console.ReadLine());
     switch (choice)
     {
         case 1:
+            //Creating Object of the class to call in Main method
+            //because method is non static
             EmployeePresence employeeAttendance = new EmployeePresence();
             employeeAttendance.EmpPresentAbsent();
             break;
@@ -18,6 +17,11 @@ while (true)
         case 2:
             EmployeeWage employeeWage = new EmployeeWage();
             employeeWage.EmpWage();
+            break;
+
+        case 3:
+            PartTime partTime = new PartTime();
+            partTime.EmployeeWage();
             break;
     }
 }
