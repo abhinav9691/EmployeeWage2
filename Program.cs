00d156be4337b9bc550c73e4976ea1b87d1a0930
Abhinav Kumar <kabhinav713@gmail.com>
2023-06-21 21:40:29 +0530

    [Abhinav]-adding wages of month

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿using EmployeeWage2;
+using System.Linq.Expressions;
 
 while (true)
 {
     Console.WriteLine("Select the choice\n1)Employee Presence\n2)Employee Daily Wage" +
-        "\n3)Part Time Wage");
+        "\n3)Part Time Wage\n4)Wage Of Month");
     int choice = Convert.ToInt32(Console.ReadLine());
     switch (choice)
     {
@@ -20,8 +21,13 @@ while (true)
             break;
 
         case 3:
-            PartTime partTime = new PartTime();
-            partTime.EmployeeWage();
+            PartTime partTimeWage = new PartTime();
+            partTimeWage.EmployeeWage();
+            break;
+        
+        case 4:
+            WagesOfMonth wageForMonth = new WagesOfMonth();
+            wageForMonth.EmployeeWage();
             break;
     }
 }
---
+++ WagesOfMonth.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage2
+{
+    public class WagesOfMonth
+    {
+        public void EmployeeWage()
+        {
+            
+            int day = 1;
+            int parttime = 1;
+            int fulltime = 2;
+            int wageperhr = 20;
+            int maxworkingdays = 20;
+            
+            int empwage = 0;
+            int emphrs = 0;
+            int totalwage = 0;
+            
+            for (day = 1; day <= maxworkingdays; day++)
+            {
+               
+                Random random = new Random();
+
+                int empcheck = random.Next(0, 3);
+
+                switch (empcheck)
+                {
+                    case 1:
+                        emphrs = 8;
+                        empwage = emphrs * wageperhr;
+                        Console.WriteLine("Employee Part Time Wage--" + empwage);
+                        totalwage += empwage;
+                        break;
+                    case 2:
+                        emphrs = 16;
+                        empwage = emphrs * wageperhr;
+                        Console.WriteLine("Employee Full Time Wage--" + empwage);
+                        totalwage += empwage;
+                        break;
+                    default:
+                        emphrs = 0;
+                        empwage = emphrs * wageperhr;
+                        Console.WriteLine("Employee is Absent");
+                        totalwage += empwage;
+                        break;
+                }
+
+
+            }
+            Console.WriteLine("Employe Total Wage for the Month--" + totalwage);
+
+        }
+    }
+}
