9674f521049278ab80b1befb45aa97203472d0f0
Abhinav Kumar <kabhinav713@gmail.com>
2023-06-21 21:11:54 +0530

    [Abhinav]-adding Empwage

---
+++ EmployeeWage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage2
+{
+    public class EmployeeWage
+    {
+        public void EmpWage()
+        {
+            int is_full_time = 1;
+            int emp_rate_per_hour = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+            Random random1 = new Random();
+            int empCheck1 = random1.Next(0, 2);
+            Console.WriteLine(empCheck1);
+
+            if (empCheck1 == is_full_time)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * emp_rate_per_hour;
+            Console.WriteLine("Emp Wage " + empWage);
+
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,5 +2,22 @@
 
 
 
-EmployeePresence employeeAttendance = new EmployeePresence();
-employeeAttendance.EmpPresentAbsent();
+
+
+while (true)
+{
+    Console.WriteLine("Select the choice\n1)Employee Presence\n2)Employee Daily Wage");
+    int choice = Convert.ToInt32(Console.ReadLine());
+    switch (choice)
+    {
+        case 1:
+            EmployeePresence employeeAttendance = new EmployeePresence();
+            employeeAttendance.EmpPresentAbsent();
+            break;
+
+        case 2:
+            EmployeeWage employeeWage = new EmployeeWage();
+            employeeWage.EmpWage();
+            break;
+    }
+}
